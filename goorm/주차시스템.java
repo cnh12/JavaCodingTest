5d6f61d65c8d0365a27abd38cfce1c9557a60d11
CNH <zetta_byte@naver.com>
2024-01-17 00:31:47 +0900

    T

--- goorm/주차시스템.java
+++ goorm/주차시스템.java
@@ -37,7 +37,6 @@ class Main {
         // 	System.out.println(Arrays.toString(arr[i]));
         // }
 
-
         for(int i = 0; i < N; i++){
             for(int j = 0; j < M; j++){
                 //탐색 시작
